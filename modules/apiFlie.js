f6af4350b2b2104582bd58ded9723fb17dfd4989
jheart-vic <adebowalevictorjheart.com>
2022-06-15 21:34:52 +0100

    Fetch API and used the data to populate the dom

---
+++ modules/apiFlie.js
@@ -0,0 +1,61 @@
+let url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/MKgk0wCou03zNTUxgPzr/scores';
+
+const setData = async () => {
+  const response = await fetch(url);
+  const scores = await response.json();
+   return scores
+};
+
+const setScores = async (player, scores) => {
+  const information = await fetch(url, {
+    method: 'post',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ user: player, score: scores }),
+  });
+  const allScore = information.json();
+  return allScore
+};
+
+export { setData, setScores };
+// let id;
+// // generating game id from api
+// fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games', {
+//   method: "post",
+//   headers: {
+//     "content-type": "application/json"
+//   },
+//   body: JSON.stringify({
+//     "name": "another cool game"
+//   })
+// })
+
+// .then((res) => {
+//   res.json().then((value) => {
+//     console.log(value);
+//     id = value.result.split(" ")[3]
+//     console.log(id)
+// })
+// })
+// posting users and score
+// fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/EoTxCV02Ftb3pgz9Cebz/scores', {
+//   method: "post",
+//   headers: {
+//     "content-type": "application/json"
+//   },
+//   body: JSON.stringify({
+//    "user": "James villa",
+//    "score": 100
+//   })
+// })
+// .then((data) => {
+//   data.json().then((val) => console.log(val))
+// })
+// getting scores
+// fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/EoTxCV02Ftb3pgz9Cebz/scores')
+// .then((data) => {
+//   data.json().then((val) => console.log(val))
+// })
+
+
--- src/index.js
+++ src/index.js
@@ -1,4 +1,27 @@
 import './style.css';
-import showAllScores from '../modules/domcontent.js';
+import {setScores} from '../modules/apiFlie.js';
+import showAllScores from '../modules/refresh';
+
+
+const submit = document.getElementById('submit')
+const playerName = document.getElementById('name')
+const playerScore = document.getElementById('score')
+const refresh = document.getElementById('refresh')
+const message = document.getElementById('errorMessage')
 
 document.addEventListener('DOMContentLoaded', showAllScores);
+refresh.addEventListener('click', showAllScores)
+submit.addEventListener('click', async (e) =>{
+ e.preventDefault()
+ if(playerName.value ===  '' || playerScore.value === ''){
+   message.style.display = 'block'
+   message.style.color = 'red'
+   setTimeout(() => {
+     message.style.display = 'none'
+    },2000);
+  }
+  await setScores(playerName.value, playerScore.value) 
+  showAllScores()
+ playerName.value = '';
+ playerScore.value = '';
+});
