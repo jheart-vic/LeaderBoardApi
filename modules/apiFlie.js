e1a6f832bfe4760386b7e6efa6a24176617e33f3
jheart-vic <adebowalevictorjheart.com>
2022-06-16 15:18:00 +0100

    Add stylings to poject

--- modules/apiFlie.js
+++ modules/apiFlie.js
@@ -1,9 +1,9 @@
-let url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/zptrWV97kHcKHhEzdCRc/scores';
+const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/zptrWV97kHcKHhEzdCRc/scores';
 
 const setData = async () => {
   const response = await fetch(url);
   const scores = await response.json();
-   return scores
+  return scores;
 };
 
 const setScores = async (player, scores) => {
@@ -15,10 +15,7 @@ const setScores = async (player, scores) => {
     body: JSON.stringify({ user: player, score: scores }),
   });
   const allScore = information.json();
-  return allScore
+  return allScore;
 };
 
 export { setData, setScores };
-
-
-
--- src/index.js
+++ src/index.js
@@ -12,19 +12,19 @@ const displayDiv = document.querySelector('#display-div');
 
 document.addEventListener('DOMContentLoaded', showAllScores);
 refresh.addEventListener('click', (e) => {
- e.preventDefault()
- if(e.target) {
-  const appreciationMessage  = document.createElement('p');
-  appreciationMessage.textContent = 'There\'s no limit to what you can achieve';
-  appreciationMessage.style.fontFamily = 'cursive';
-  appreciationMessage.style.color = 'rgb(0, 168,132)';
-  appreciationMessage.style.alignSelf = 'flex-end'
-  appreciationMessage.style.fontSize = '27px'
-  displayDiv.insertBefore(appreciationMessage, displayDiv[0]);
-  setTimeout(()=> {
-    appreciationMessage.style.display = 'none';
-  }, 5000)
- }
+  e.preventDefault();
+  if (e.target) {
+    const appreciationMessage = document.createElement('p');
+    appreciationMessage.textContent = 'There\'s no limit to what you can achieve';
+    appreciationMessage.style.fontFamily = 'cursive';
+    appreciationMessage.style.color = 'rgb(0, 168,132)';
+    appreciationMessage.style.alignSelf = 'flex-end';
+    appreciationMessage.style.fontSize = '27px';
+    displayDiv.insertBefore(appreciationMessage, displayDiv[0]);
+    setTimeout(() => {
+      appreciationMessage.style.display = 'none';
+    }, 5000);
+  }
   showAllScores();
 });
 submit.addEventListener('click', async (e) => {
@@ -33,7 +33,7 @@ submit.addEventListener('click', async (e) => {
     message.innerText = 'please add valid user and score';
     message.style.display = 'block';
     message.style.color = 'red';
-    load.style.animation = '5s load linear'
+    load.style.animation = '5s load linear';
     load.style.background = 'red';
     setTimeout(() => {
       message.style.display = 'none';
@@ -42,16 +42,16 @@ submit.addEventListener('click', async (e) => {
   }
 
   if (playerName.value !== '' && playerScore.value !== '') {
-     message.innerText = 'Your score has been added, click refresh to see score';
-     message.style.color = 'rgb(0, 168,132)';
-     message.style.display = 'block';
-     load.style.animation = '5s load linear';
-     load.style.background = 'rgb(0, 168,132)';
+    message.innerText = 'Your score has been added, click refresh to see score';
+    message.style.color = 'rgb(0, 168,132)';
+    message.style.display = 'block';
+    load.style.animation = '5s load linear';
+    load.style.background = 'rgb(0, 168,132)';
     setTimeout(() => {
       message.style.display = 'none';
       load.style.animation = '';
     }, 5000);
-  } 
+  }
   await setScores(playerName.value, playerScore.value);
   playerName.value = '';
   playerScore.value = '';
