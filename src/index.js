7931362769c8aa2922da59e7e9955e1287bfd100
jheart-vic <adebowalevictorjheart.com>
2022-06-23 08:45:17 +0100

    Fix linter errror

--- src/index.js
+++ src/index.js
@@ -39,7 +39,7 @@ submit.addEventListener('click', async (e) => {
       message.style.display = 'none';
       load.style.animation = '';
     }, 5000);
-  }else {
+  } else {
     message.innerText = 'Your score has been added, click refresh to see score';
     message.style.color = 'rgb(0, 168,132)';
     message.style.display = 'block';
